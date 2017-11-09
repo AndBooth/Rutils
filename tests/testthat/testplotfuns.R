72391a5d51849e5ba5811b56b88ce8b276d7bb2e
AndBooth <andrew@elastacloud.com>
2017-11-09 16:23:02 +0000

    added test for save_plot function

--- tests/testthat/testplotfuns.R
+++ tests/testthat/testplotfuns.R
@@ -1,9 +1,13 @@
 
 context("Testing plotting functions")
+library(ggplot2)
 
 
 test_that("Plotting function works correctly", {
 
+  plot1 <- ggplot(mtcars, aes(cyl, mpg))
+  save_plot_png(plot1, "testplot")
 
+  expect_true(file.exists("./Plots/testplot.png"))
 
 })
